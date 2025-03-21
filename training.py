9ac9cf8d12750d9d1d2df8798c7d4e8b9334d83e
Kajetan Bilski <kajetan.bilski@pwr.edu.pl>
2025-03-21 16:18:55 +0100

    made it faster

--- training.py
+++ training.py
@@ -1,25 +1,52 @@
 # %%
 import os
+from typing import List
 
 os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 os.environ["WANDB_PROJECT"]="molecular-fingerprinting"
 
 # %%
 from datasets import DatasetDict, Value
+import torch
 from torch import nn
+from torch.utils.data import random_split, Dataset
 from transformers import Trainer, TrainingArguments, EarlyStoppingCallback
 import evaluate
 import wandb
 
 # %%
-ds = DatasetDict.load_from_disk('data/dataset')
+# ds = DatasetDict.load_from_disk('data/dataset')
 
-ds = ds.rename_column('data', 'inputs')
-ds = ds.remove_columns('sex_label')
-ds = ds.cast_column('label', Value('float64'))
+# ds = ds.rename_column('data', 'inputs')
+# ds = ds.remove_columns('sex_label')
+# ds = ds.cast_column('label', Value('float64'))
 
 # %%
-ds['train'][0]
+class LabeledDataset(Dataset):
+    def __init__(self, ds: Dataset, active_label: int = 1):
+        super().__init__()
+        self.ds = ds
+        self.active_label = active_label
+    
+    def __getitem__(self, index):
+        sample = self.ds.__getitem__(index)
+        return {'inputs': sample[0], 'labels': sample[self.active_label]}
+    
+    def __len__(self):
+        return len(self.ds)
+
+
+ds = torch.load('data/dataset.pt', weights_only=False)
+
+ds = LabeledDataset(ds)
+
+generator = torch.Generator().manual_seed(42)
+splits = random_split(ds, [.8, .1, .1], generator)
+ds = {
+    'train': splits[0],
+    'dev': splits[1],
+    'test': splits[2]
+}
 
 # %%
 class CustomModel(nn.Module):
@@ -40,26 +67,31 @@ class CustomModel(nn.Module):
     
     def forward(self, inputs, labels=None):
         # print(inputs, labels)
+        inputs = inputs.to(self._dtype())
         logits = self.net(inputs).squeeze()
         if labels is not None:
+            labels = labels.to(logits.dtype)
             loss = self.loss_fn(logits, labels)
             return {"loss": loss, "logits": logits}
         return {"logits": logits}
+    
+    def _dtype(self):
+        return self.net[1].weight.dtype
 
 model = CustomModel().cuda()
 
 # %%
 training_args = TrainingArguments(
     output_dir='models',
-    per_device_train_batch_size=3200,
-    per_device_eval_batch_size=3200,
+    per_device_train_batch_size=1024,
+    per_device_eval_batch_size=1024,
     learning_rate=1e-3,
     num_train_epochs=100,
     weight_decay=.01,
     eval_strategy='epoch',
     save_strategy='epoch',
     load_best_model_at_end=True,
-    bf16=True,
+    # bf16=True,
     dataloader_pin_memory=True,
     dataloader_num_workers=12,
     report_to='wandb'
@@ -88,9 +120,8 @@ trainer = Trainer(
 trainer.train()
 
 # %%
-trainer.evaluate(ds['test'])
+trainer.evaluate(ds['test'], metric_key_prefix='test')
+trainer.save_model('models/trained_model')
 
 # %%
 wandb.finish()
-
-
